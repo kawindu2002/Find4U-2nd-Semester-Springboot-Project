c53757c191cf1d28c281893a92271ccf89ca07d6
kawindu2002 <kawindupriyashan@gmail.com>
2025-09-09 01:23:43 +0530

    added MissingTipDTO

---
+++ src/main/java/com/find4u/find4u2ndsemesterspringbootproject/dto/MissingTipDTO.java
@@ -0,0 +1,24 @@
+package com.find4u.find4u2ndsemesterspringbootproject.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MissingTipDTO {
+     private Long id;
+     
+     @JsonFormat(pattern = "yyyy-MM-dd")
+     private LocalDate date;
+     private String tip;
+     private String source;
+     
+     private String missingPersonId;
+     
+}
+
