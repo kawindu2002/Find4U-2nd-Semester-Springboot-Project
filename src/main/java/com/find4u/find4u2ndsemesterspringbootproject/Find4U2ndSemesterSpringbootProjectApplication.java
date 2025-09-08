bc7c5f3d71c20855c47d0ee66cbaf58fb1d2c392
kawindu2002 <kawindupriyashan@gmail.com>
2025-09-09 01:08:34 +0530

    added model mapper

--- src/main/java/com/find4u/find4u2ndsemesterspringbootproject/Find4U2ndSemesterSpringbootProjectApplication.java
+++ src/main/java/com/find4u/find4u2ndsemesterspringbootproject/Find4U2ndSemesterSpringbootProjectApplication.java
@@ -1,7 +1,9 @@
 package com.find4u.find4u2ndsemesterspringbootproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Find4U2ndSemesterSpringbootProjectApplication {
@@ -10,4 +12,9 @@ public class Find4U2ndSemesterSpringbootProjectApplication {
           SpringApplication.run(Find4U2ndSemesterSpringbootProjectApplication.class, args);
      }
      
+     @Bean
+     public ModelMapper modelMapper() {
+          return new ModelMapper();
+     }
 }
+
