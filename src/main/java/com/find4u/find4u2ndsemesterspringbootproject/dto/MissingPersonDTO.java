0f3349026808fec9809caed99a077980e73b9ea8
kawindu2002 <kawindupriyashan@gmail.com>
2025-09-09 01:23:07 +0530

    added MissingPersonDTO

---
+++ src/main/java/com/find4u/find4u2ndsemesterspringbootproject/dto/MissingPersonDTO.java
@@ -0,0 +1,59 @@
+
+package com.find4u.find4u2ndsemesterspringbootproject.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kp.find4uspringbootproject.enums.Gender;
+import com.kp.find4uspringbootproject.enums.PersonStatus;
+import com.kp.find4uspringbootproject.enums.YesNo;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MissingPersonDTO {
+     
+     private String id;   // MIS-2025-00001
+     private String fullName;
+     private Integer age;
+     private Gender gender;
+     private String height;
+     private String weight;
+     private String hairColor;
+     private String eyeColor;
+     private String distinguishingFeatures;
+     private String photoUrl;
+     private PersonStatus status;
+     
+     @JsonFormat(pattern = "yyyy-MM-dd")
+     private LocalDate missingDate;
+     @JsonFormat(pattern = "hh:mm a")  // 'hh' = 12-hour format | 'a' = AM/PM
+     private LocalTime missingTime;
+     
+     private String location;
+     private String locationLatitude;
+     private String locationLongitude;
+     
+     private String nearByLandmarks;
+     private String clothing;
+     private String circumstances;
+     
+     private String reporterId;    // FK only reporter's ID = User id --> username, email and contact number
+     private String relationToMissingPerson; // Ex: Brother, Sister
+     private String reward;
+     
+     private YesNo acceptedTermPolicy;
+     private YesNo confirmedDetails;
+     
+     private LocalDateTime createdAt;
+     private LocalDateTime updatedAt;
+     
+}
+
+// In map, We send the location and gets its coordinates for the map using Ajax GET.
+
