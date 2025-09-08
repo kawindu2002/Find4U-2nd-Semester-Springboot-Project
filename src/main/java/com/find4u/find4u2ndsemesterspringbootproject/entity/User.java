16905423b54ad1f5ed72628673132c9d4b02896a
kawindu2002 <kawindupriyashan@gmail.com>
2025-09-09 01:27:01 +0530

    added User entity

---
+++ src/main/java/com/find4u/find4u2ndsemesterspringbootproject/entity/User.java
@@ -0,0 +1,49 @@
+package com.find4u.find4u2ndsemesterspringbootproject.entity;
+
+import com.kp.find4uspringbootproject.enums.Role;
+import com.kp.find4uspringbootproject.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user")
+public class User {
+     @Id
+     private String id; // e.g., USR-2025-00001
+     
+     private String name;
+     
+     @Column(unique = true)
+     private String email;
+     
+     @Column(name = "phone_number")
+     private String phoneNumber;
+     
+     @Enumerated(EnumType.STRING)
+     private Role role = Role.USER;;  // 'user', 'admin'
+     
+     @Enumerated(EnumType.STRING)
+     private UserStatus status = UserStatus.ACTIVE;   // 'active', 'inactive'
+     
+     @Column(name = "last_login")
+     private LocalDateTime lastLogin;
+     
+     @CreationTimestamp
+     @Column(name = "created_at",updatable = false)
+     private LocalDateTime createdAt;
+     
+     @UpdateTimestamp
+     @Column(name = "updated_at")
+     private LocalDateTime updatedAt;
+     
+}
+
