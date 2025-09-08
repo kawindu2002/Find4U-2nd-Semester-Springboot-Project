ec9525ea484050a2e3cb2a71db979f126943262f
kawindu2002 <kawindupriyashan@gmail.com>
2025-09-09 01:15:55 +0530

    added faqController

---
+++ src/main/java/com/find4u/find4u2ndsemesterspringbootproject/controller/faqController.java
@@ -0,0 +1,31 @@
+package com.find4u.find4u2ndsemesterspringbootproject.controller;
+
+import com.kp.find4uspringbootproject.dto.APIResponse;
+import com.kp.find4uspringbootproject.dto.FaqDTO;
+import com.kp.find4uspringbootproject.service.FaqService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/find4u/faq")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class faqController {
+     
+     private final FaqService faqService;
+     
+     // Get all
+     @GetMapping("getAll")
+     public ResponseEntity<APIResponse> getAll() {
+          List<FaqDTO> faqList = faqService.getAllFaqs();
+          return new ResponseEntity(new APIResponse(200, "Success", faqList), HttpStatus.OK);
+     }
+}
+
